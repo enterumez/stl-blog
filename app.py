ea4ca3c4656874efb3dc7e78c90c1303952b7246
enterumez <131345035+enterumez@users.noreply.github.com>
2024-07-05 03:39:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -113,12 +113,10 @@ elif choice == "View Posts":
     st.title("View Posts")
     st.write("Here you can see all the posts in the blog.")
     posts = get_all_posts()
-# Display each post as a card
-for i, post in enumerate(posts):
-    st.markdown(title_temp.format(post[1], post[0], post[2][:50] + "..."), unsafe_allow_html=True)
-    if st.button(f"Read More###{i}", key=post[1]):
-        st.markdown(post_temp.format(post[1], post[0], post[3], post[2]), unsafe_allow_html=True)
-
+    for i, post in enumerate(posts):
+        st.markdown(title_temp.format(post[1], post[0], post[2][:50] + "..."), unsafe_allow_html=True)
+        if st.button(f"Read More###{i}", key=post[1]):
+            st.markdown(post_temp.format(post[1], post[0], post[3], post[2]), unsafe_allow_html=True)
 elif choice == "Add Post":
     st.title("Add Post")
     st.write("Here you can add a new post to the blog.")
