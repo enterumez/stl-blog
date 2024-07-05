d8004a9820784d007ea8e11662c1a568e8ce707a
enterumez <131345035+enterumez@users.noreply.github.com>
2024-07-06 01:58:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import sqlite3
 import streamlit as st
 import pandas as pd
-from PIL import Image
 
 # データベースファイルの名前
 database = 'blog.db'
@@ -105,8 +104,7 @@ if choice == "Home":
     st.title("Welcome to my blog")
     st.write("This is a simple blog app built with streamlit and python.")
     st.write("You can view, add, and manage posts using the sidebar menu.")
-    image = Image.open('Image/図2.png')
-    st.image(image)
+
 elif choice == "View Posts":
     st.title("View Posts")
     st.write("Here you can see all the posts in the blog.")
